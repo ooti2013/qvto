75dde5a08b36ab9e9c105924de27cfbb5a42b5d2
Favio Bettiol <fbettiol@tue.nl>
2014-11-24 14:43:55 +0100

    adding empty methods.

--- examples/org.eclipse.qvto.examples.pivot.trad2pivot.java/src/org/eclipse/qvto/examples/pivot/trad2pivot/java/QVTo2PivotQVTo/Qvto2PivotQvto.java
+++ examples/org.eclipse.qvto.examples.pivot.trad2pivot.java/src/org/eclipse/qvto/examples/pivot/trad2pivot/java/QVTo2PivotQVTo/Qvto2PivotQvto.java
@@ -10,8 +10,28 @@
  *******************************************************************************/
 package org.eclipse.qvto.examples.pivot.trad2pivot.java.QVTo2PivotQVTo;
 
+import org.eclipse.m2m.internal.qvt.oml.ast.parser.QvtOperationalValidationVisitor;
+import org.eclipse.m2m.internal.qvt.oml.ast.parser.QvtOperationalVisitorCS;
+import org.eclipse.m2m.internal.qvt.oml.compiler.QvtCompilerOptions;
+import org.eclipse.m2m.internal.qvt.oml.cst.parser.AbstractQVTParser;
+import org.eclipse.qvto.examples.pivot.qvtoperational.EntryOperation;
+import org.eclipse.qvto.examples.pivot.qvtoperational.Helper;
+import org.eclipse.qvto.examples.pivot.qvtoperational.ImperativeCallExp;
+import org.eclipse.qvto.examples.pivot.qvtoperational.ImperativeOperation;
+import org.eclipse.qvto.examples.pivot.qvtoperational.Library;
+import org.eclipse.qvto.examples.pivot.qvtoperational.MappingBody;
+import org.eclipse.qvto.examples.pivot.qvtoperational.MappingCallExp;
+import org.eclipse.qvto.examples.pivot.qvtoperational.MappingOperation;
+import org.eclipse.qvto.examples.pivot.qvtoperational.MappingParameter;
+import org.eclipse.qvto.examples.pivot.qvtoperational.ModelParameter;
+import org.eclipse.qvto.examples.pivot.qvtoperational.ModelType;
 import org.eclipse.qvto.examples.pivot.qvtoperational.Module;
+import org.eclipse.qvto.examples.pivot.qvtoperational.ModuleImport;
+import org.eclipse.qvto.examples.pivot.qvtoperational.ObjectExp;
+import org.eclipse.qvto.examples.pivot.qvtoperational.OperationBody;
 import org.eclipse.qvto.examples.pivot.qvtoperational.OperationalTransformation;
+import org.eclipse.qvto.examples.pivot.qvtoperational.ResolveExp;
+import org.eclipse.qvto.examples.pivot.qvtoperational.VarParameter;
 
 
 public class Qvto2PivotQvto {
@@ -23,8 +43,7 @@ public class Qvto2PivotQvto {
 		ecoreToPivot = new Ecore2Pivot ();
 	}
 	
-	
-	
+		
 	public void toOperationalTransformation (org.eclipse.m2m.internal.qvt.oml.expressions.OperationalTransformation input, OperationalTransformation output)
 	{
 		//output.getIntermediateClass().add(.......)
@@ -36,10 +55,73 @@ public class Qvto2PivotQvto {
 	{
 		//toEntryOperation (input.getEntry(),output.getEntry());  
 		ecoreToPivot.toClass (input, output);
-		
 	}
 	
+	public void toEntryOperation (org.eclipse.m2m.internal.qvt.oml.expressions.EntryOperation input,  EntryOperation output)
+	{
+	}
 	
+	public void toHelper (org.eclipse.m2m.internal.qvt.oml.expressions.Helper input, Helper output)
+	{
+	}
+	
+	public void toImperativeCallExp (org.eclipse.m2m.internal.qvt.oml.expressions.ImperativeCallExp input,  ImperativeCallExp output)
+	{
+	}
+	
+	public void toImperativeOperation (org.eclipse.m2m.internal.qvt.oml.expressions.ImperativeOperation input,  ImperativeOperation output)
+	{
+	}
+	
+	public void toLibrary (org.eclipse.m2m.internal.qvt.oml.expressions.Library input,  Library output)
+	{
+	}
+	
+	public void toMappingBody (org.eclipse.m2m.internal.qvt.oml.expressions.MappingBody input,  MappingBody output)
+	{
+	}
+	
+	public void toMappingCallExp (org.eclipse.m2m.internal.qvt.oml.expressions.MappingCallExp input,  MappingCallExp output)
+	{
+	}
+	
+	public void toMappingOperation (org.eclipse.m2m.internal.qvt.oml.expressions.MappingOperation input,  MappingOperation output)
+	{
+	}
+	
+	public void toMappingParameter (org.eclipse.m2m.internal.qvt.oml.expressions.MappingParameter input,  MappingParameter output)
+	{
+	}
+	
+	public void toModelParameter (org.eclipse.m2m.internal.qvt.oml.expressions.ModelParameter input, ModelParameter  output)
+	{
+	}
+	
+	public void toModelType (org.eclipse.m2m.internal.qvt.oml.expressions.ModelType input,  ModelType output)
+	{
+	}
+	
+	public void toModuleImport (org.eclipse.m2m.internal.qvt.oml.expressions.ModuleImport input,  ModuleImport output)
+	{
+	}
+	
+	public void toObjectExp (org.eclipse.m2m.internal.qvt.oml.expressions.ObjectExp input,  ObjectExp output)
+	{
+	}
+	
+	public void toOperationBody (org.eclipse.m2m.internal.qvt.oml.expressions.OperationBody input,  OperationBody output)
+	{
+	}
+	
+	public void toResolveExp (org.eclipse.m2m.internal.qvt.oml.expressions.ResolveExp input, ResolveExp output)
+	{
+	}
+	
+	public void toVarParameter (org.eclipse.m2m.internal.qvt.oml.expressions.VarParameter input,  VarParameter output)
+	{
+	}
 }
+
+
 
 
