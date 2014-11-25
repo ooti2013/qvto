19c0b5b9c78508f79a0355c3dce5133fd7a47463
Tesfahun Tesfay <ttesfay@tue.nl>
2014-11-25 15:31:30 +0100

    QVTo2PivotQVTo/Qvto2PivotQvto.java modified

--- examples/org.eclipse.qvto.examples.pivot.trad2pivot.java/src/org/eclipse/qvto/examples/pivot/trad2pivot/java/QVTo2PivotQVTo/Qvto2PivotQvto.java
+++ examples/org.eclipse.qvto.examples.pivot.trad2pivot.java/src/org/eclipse/qvto/examples/pivot/trad2pivot/java/QVTo2PivotQVTo/Qvto2PivotQvto.java
@@ -134,7 +134,7 @@ public class Qvto2PivotQvto {
 			org.eclipse.m2m.internal.qvt.oml.expressions.MappingOperation input,
 			MappingOperation output) {
 		
-		toImperativeOperation(input, output);
+		//toImperativeOperation(input, output);
 
 		for (org.eclipse.m2m.internal.qvt.oml.expressions.MappingOperation element : input.getDisjunct()) {	
 			MappingOperation res = null;
