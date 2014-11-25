b0ecf39c108b20b74b48e25633ef6928f788583d
Arash Shafiei <arash.shafiei@gmail.com>
2014-11-25 09:27:34 +0100

    add dispatcher and OCL2Pivot

---
+++ examples/org.eclipse.qvto.examples.pivot.trad2pivot.java/src/org/eclipse/qvto/examples/pivot/trad2pivot/java/QVTo2PivotQVTo/Dispatcher.java
@@ -0,0 +1,35 @@
+package org.eclipse.qvto.examples.pivot.trad2pivot.java.QVTo2PivotQVTo;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.ocl.examples.pivot.Class;
+import org.eclipse.ocl.examples.pivot.manager.MetaModelManager;
+import org.eclipse.qvto.examples.pivot.qvtoperational.OperationalTransformation;
+import org.eclipse.qvto.examples.pivot.qvtoperational.QVTOperationalFactory;
+import org.eclipse.qvto.examples.pivot.trad2pivot.java.QVTo2PivotQVTo.Qvto2PivotQvto;
+
+public class Dispatcher {
+
+	private Qvto2PivotQvto qvto2pivot;
+	private QVTOperationalFactory factory = QVTOperationalFactory.eINSTANCE;
+	
+	Dispatcher ()
+	{
+
+	}
+	
+	private Class classDispatcher(EClass input) {
+
+		if (input instanceof org.eclipse.m2m.internal.qvt.oml.expressions.OperationalTransformation) {
+			OperationalTransformation res = factory
+					.createOperationalTransformation();
+			qvto2pivot.toOperationalTransformation(
+					(org.eclipse.m2m.internal.qvt.oml.expressions.OperationalTransformation) input,
+					res);
+			return res;
+		}
+
+		return null;
+	}
+
+}
+
