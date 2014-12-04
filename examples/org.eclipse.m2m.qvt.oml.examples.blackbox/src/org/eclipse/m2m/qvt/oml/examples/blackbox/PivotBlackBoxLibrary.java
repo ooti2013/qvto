63e45768a1696bc31792a9d648a17743be49fff2
Tesfahun Tesfay <ttesfay@tue.nl>
2014-12-04 09:43:50 +0100

    getResolvedOperationTest added to PivotBlackBoxLibrary.java

--- examples/org.eclipse.m2m.qvt.oml.examples.blackbox/src/org/eclipse/m2m/qvt/oml/examples/blackbox/PivotBlackBoxLibrary.java
+++ examples/org.eclipse.m2m.qvt.oml.examples.blackbox/src/org/eclipse/m2m/qvt/oml/examples/blackbox/PivotBlackBoxLibrary.java
@@ -17,49 +17,73 @@ import org.eclipse.emf.ecore.EParameter;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.m2m.qvt.oml.blackbox.java.Operation;
 import org.eclipse.ocl.examples.pivot.Class;
+import org.eclipse.ocl.examples.pivot.IntegerLiteralExp;
 import org.eclipse.ocl.examples.pivot.Parameter;
 import org.eclipse.ocl.examples.pivot.ParserException;
+import org.eclipse.ocl.examples.pivot.PivotFactory;
 import org.eclipse.ocl.examples.pivot.Property;
 import org.eclipse.ocl.examples.pivot.Type;
 import org.eclipse.ocl.examples.pivot.manager.MetaModelManager;
 
 public class PivotBlackBoxLibrary {
-	
+
 	private static MetaModelManager metaModelManager;
-	
+
 	public PivotBlackBoxLibrary() {
 		super();
 		metaModelManager = new MetaModelManager();
 	}
 
-	@Operation(contextual=true)
+	@Operation(contextual = true)
 	public static Object getClass(EClass self) throws ParserException {
 		return metaModelManager.getPivotOf(Class.class, self);
 	}
 
-	@Operation(contextual=true)
+	@Operation(contextual = true)
 	public static Object getType(EClassifier self) throws ParserException {
 		return metaModelManager.getPivotOf(Type.class, self);
 	}
-	
-	@Operation(contextual=true)
-	public static Object getProperty(EStructuralFeature self) throws ParserException {
+
+	@Operation(contextual = true)
+	public static Object getProperty(EStructuralFeature self)
+			throws ParserException {
 		return metaModelManager.getPivotOf(Property.class, self);
 	}
-	
-	@Operation(contextual=true)
+
+	@Operation(contextual = true)
 	public static Object getOperation(EOperation self) throws ParserException {
-		return metaModelManager.getPivotOf(org.eclipse.ocl.examples.pivot.Operation.class, self);
+		return metaModelManager.getPivotOf(
+				org.eclipse.ocl.examples.pivot.Operation.class, self);
 	}
-	
-	@Operation(contextual=true)
+
+	@Operation(contextual = true)
 	public static Object getParameter(EParameter self) throws ParserException {
 		return metaModelManager.getPivotOf(Parameter.class, self);
 	}
-		
-	@Operation(contextual=true)
-	public static Object getIntegerType (EClassifier self)
-	{
+
+	@Operation(contextual = true)
+	public static Object getIntegerType(EClassifier self) {
 		return metaModelManager.getIntegerType();
 	}
+
+	@Operation(contextual = true)
+	public static Object getResolvedOperationTest() throws Exception {
+		// creating pivot operation
+		org.eclipse.ocl.examples.pivot.Operation result = PivotFactory.eINSTANCE
+				.createOperation();
+		// creating a rightside
+		IntegerLiteralExp rightside = PivotFactory.eINSTANCE
+				.createIntegerLiteralExp();
+		rightside.setType(PivotFactory.eINSTANCE.createType());
+		// creating a leftside
+		IntegerLiteralExp leftside = PivotFactory.eINSTANCE
+				.createIntegerLiteralExp();
+		leftside.setType(PivotFactory.eINSTANCE.createType());
+		// calling the resolveOperation of the metaModelManager
+		result = metaModelManager.resolveOperation(leftside.getType(), ">",
+				rightside.getType());
+
+		return result;
+	}
+
 }
